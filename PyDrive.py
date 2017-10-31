a41c4ae0233e70b96902d67505a285572898fe1b
TheKingSparta <zachclow2013@gmail.com>
2017-10-31 13:33:44 -0700

    Create PyDrive.py

---
+++ PyDrive.py
@@ -0,0 +1,23 @@
+#!Python36
+#Shebang!
+
+#Import libraries
+import os
+import oauth2client
+import ZipFile
+#/Libs
+
+def Upload(file):
+
+def Download(file):
+
+def Zip(file):
+
+def Unzip(file, destination):
+	with ZipFile(file) as project:
+		with project.extractall(destination)
+	
+	
+def GetProject(unzip):
+
+def PushProject(zip):
